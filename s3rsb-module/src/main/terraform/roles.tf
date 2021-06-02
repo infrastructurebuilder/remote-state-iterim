d9882769c1f4ac01710a9837aeb7bda217731a15
Mykel Alvis <mykel.alvis@gmail.com>
2021-06-02 09:12:22 -0500

    Fix policy naming

--- s3rsb-module/src/main/terraform/roles.tf
+++ s3rsb-module/src/main/terraform/roles.tf
@@ -48,7 +48,7 @@ data "aws_iam_policy_document" "write_state_bucket" {
 resource "aws_iam_policy" "read_state_bucket_policy" {
   name   = "rsb-${aws_s3_bucket.rsbbucket.name}-read"
   path   = "/"
-  policy = data.aws_iam_policy_document.read_bucket.json
+  policy = data.aws_iam_policy_document.read_state_bucket.json
   tags = {
 #    Name        = "rsb-${var.s3_bucket_name}-read"
     Owner       = "${var.owner}"
@@ -65,7 +65,7 @@ resource "aws_iam_policy" "read_state_bucket_policy" {
 resource "aws_iam_policy" "write_state_bucket_policy" {
   name   = "rsb-${aws_s3_bucket.rsbbucket.name}-write"
   path   = "/"
-  policy = data.aws_iam_policy_document.write_bucket.json
+  policy = data.aws_iam_policy_document.write_state_bucket.json
   tags = {
 #    Name        = "rsb-${var.s3_bucket_name}-write"
     Owner       = "${var.owner}"
@@ -81,7 +81,7 @@ resource "aws_iam_policy" "write_state_bucket_policy" {
 resource "aws_iam_role" "read_state_role" {
   name               = "rsb-${var.s3_bucket_name}-read"
   path               = "/@state.target.prefix@"
-  assume_role_policy = data.aws_iam_policy_document.read_bucket_policy.json
+  assume_role_policy = data.aws_iam_policy_document.read_state_bucket.json
   tags = {
 #    Name        = "${var.thisproject} Read Role"
     Owner       = "${var.owner}"
@@ -97,7 +97,7 @@ resource "aws_iam_role" "read_state_role" {
 resource "aws_iam_role" "write_state_role" {
   name               = "rsb-${var.s3_bucket_name}-write"
   path               = "/@state.target.prefix@"
-  assume_role_policy = data.aws_iam_policy_document.read_bucket_policy.json
+  assume_role_policy = data.aws_iam_policy_document.write_state_bucket.json
   tags = {
     Owner       = "${var.owner}"
     Purpose     = "Allows writes to ${var.s3_bucket_name} State Bucket"
@@ -133,7 +133,7 @@ data "aws_iam_policy_document" "read_logging_bucket" {
   }
 }
 
-data "aws_iam_policy_document" "write_state_bucket" {
+data "aws_iam_policy_document" "write_logging_bucket" {
   statement {
     sid = "1"
     actions = [
@@ -160,7 +160,7 @@ data "aws_iam_policy_document" "write_state_bucket" {
 resource "aws_iam_policy" "read_logging_bucket_policy" {
   name   = "rsb-${aws_s3_bucket.logbucket.name}-read"
   path   = "/"
-  policy = data.aws_iam_policy_document.read_bucket.json
+  policy = data.aws_iam_policy_document.read_logging_bucket.json
   tags = {
 #    Name        = "rsb-${var.s3_bucket_name}-read"
     Owner       = "${var.owner}"
@@ -177,7 +177,7 @@ resource "aws_iam_policy" "read_logging_bucket_policy" {
 resource "aws_iam_policy" "write_logging_bucket_policy" {
   name   = "rsb-${aws_s3_bucket.logbucket.name}-write"
   path   = "/"
-  policy = data.aws_iam_policy_document.write_bucket.json
+  assume_role_policy = data.aws_iam_policy_document.write_logging_bucket.json
   tags = {
     Owner       = "${var.owner}"
     Purpose     = "${var.thispurpose}"
@@ -192,7 +192,7 @@ resource "aws_iam_policy" "write_logging_bucket_policy" {
 resource "aws_iam_role" "read_logging_role" {
   name               = "rsb-${var.s3_bucket_name}-log-read"
   path               = "/@logging.target.prefix@"
-  assume_role_policy = data.aws_iam_policy_document.read_bucket_policy.json
+  assume_role_policy = data.aws_iam_policy_document.read_logging_bucket.json
   tags = {
 #    Name        = "${var.thisproject} Read Role"
     Owner       = "${var.owner}"
@@ -208,7 +208,7 @@ resource "aws_iam_role" "read_logging_role" {
 resource "aws_iam_role" "write_logging_role" {
   name               = "rsb-${var.s3_bucket_name}-log-write"
   path               = "/@logging.target.prefix@"
-  assume_role_policy = data.aws_iam_policy_document.read_bucket_policy.json
+  assume_role_policy = data.aws_iam_policy_document.write_logging_bucket.json
   tags = {
     Owner       = "${var.owner}"
     Purpose     = "Allows writes to ${var.s3_bucket_name}-log Bucket"
